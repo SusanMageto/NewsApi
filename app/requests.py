29f37aabf44cef1d939e6fc02c01b5b31bf3fb6e
SusanMageto <susan.mageto@student.moringaschool.com>
2022-05-02 17:03:16 +0300

    adds the news urls to the request.py file

--- app/requests.py
+++ app/requests.py
@@ -1,14 +1,21 @@
 import urllib.request,json
+
+
 from .models import sources
-from .models import headlines
+from .models import articles
 
 
 # Getting api key
 api_key = None
-# Getting the movie base url
-base_url = None
+# Getting the news base url
+sources_url = None
+headlines_url=None
+search_url=None
 
 def configure_request(app):
     global api_key,base_url
-    api_key = app.config['MOVIE_API_KEY']
-    base_url = app.config['MOVIE_API_BASE_URL']
+    api_key = app.config['NEWS_API_KEY']
+    sources_url=app.config['SOURCES_API_URL']
+    headlines_url=app.config['HEADLINES_API_URL']
+    search_url=app.config['SEARCH_API_URL']
+    
