8a27cfeff72648b9d3ce29b6d84cbe13dbd820fc
SusanMageto <susan.mageto@student.moringaschool.com>
2022-05-02 16:16:20 +0300

    adds bootstrap and initializes it

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+
 
 def create_app(config_name):
     app = Flask(__name__)
+  
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    # app.config.from_object(DevConfig)
+    # app.config.from_pyfile("config.py")
+
 
     # Initializing flask extensions
     bootstrap.init_app(app)
@@ -13,5 +22,7 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    
 
     return app
+  
