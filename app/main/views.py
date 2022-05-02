2895b703dc6e73f96be6fc42a58e0f545c1101bc
SusanMageto <susan.mageto@student.moringaschool.com>
2022-05-03 02:12:57 +0300

    updates the errors and views file in the main folder

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,27 @@
-from flask import render_template
-from app import app
+from flask import render_template, redirect, url_for
+from . import main
+from ..request import get_articles, get_sources
 
-# Views
-@app.route('/')
+
+@main.route('/')
 def index():
+    title = 'Home - Welcome to News Article'
+    sports = get_sources('sports')
+    business = get_sources('business')
+    entertainment = get_sources('entertainment')
+    general = get_sources('general')
+    health = get_sources('health')
+    science = get_sources('science')
+    technology = get_sources('technology')
+    
+    return render_template('index.html', title=title, sports=sports,business=business,
+                           technology=technology,entertainment=entertainment,
+                           general=general,health=health,science=science,)
 
-  '''
-  View root page function that returns the index page and its data
-  '''
 
-  return render_template('index.html')
+@main.route('/news/<id>')
+def articles(id):
+
+    articles_display = get_articles(id)
+    title=f'{id}'
+    return render_template('news.html',articles=articles_display,)
