ceeebf8d4575f345c5ff329df2aec6e844c8bf62
SusanMageto <susan.mageto@student.moringaschool.com>
2022-05-02 16:48:18 +0300

    adds two classes(articles and sources) to the models file

--- app/models.py
+++ app/models.py
@@ -1,1 +1,28 @@
-class 
+class sources:
+  '''
+  class that defines sources
+  '''
+  def __init__(self,id,name,description,url,category,language,country):
+      self.id=id
+      self.name=name
+      self.description=description
+      self.url=url
+      self.category=category
+      self.language=language
+      self.country=country
+      
+class articles:
+  '''
+  class that defines articles
+  '''
+  def __init__(self,source,id,name,author,title,description,url,urlToImage,publishedAt,content):
+    self.source=source
+    self.id=id
+    self.name=name
+    self.author=author
+    self.title=title
+    self.description=description
+    self.url=url
+    self.urlToImage=urlToImage
+    self.publishedAt=publishedAt
+    self.content=content
